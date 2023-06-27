fec6076f91a065d58a7e4bdc9d7da70ae7cd6ac8
Luciano da Silva Martins <zicadopv@gmail.com>
2023-06-27 13:07:14 -0300

    Alterando a autenticação por TOKEN

--- backend/settings.py
+++ backend/settings.py
@@ -95,9 +95,9 @@ REST_FRAMEWORK = {
     # "DEFAULT_PERMISSION_CLASSES": (
     #     "rest_framework.permissions.IsAuthenticated",
     # ),
-    # "DEFAULT_AUTHENTICATION_CLASSES": (
-    #     "rest_framework.authentication.TokenAuthentication",
-    # ),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
 }
 
 LANGUAGE_CODE = "pt-br"
